41037d486a37cbd626080294d12a7fb3b7bf4630
Sema Topcu <ssemaatopcu.@gmail.com>
2024-06-18 20:09:20 +0300

    adding more switches

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -10,19 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var colorView: UIView!
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.backgroundColor = .black
+        updateColor()
     }
-
+    @IBOutlet var blueSwitch: UISwitch!
+    
 
     @IBAction func switchChanged(_ sender: Any) {
-        if (sender as AnyObject).isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
-        }
+        updateColor()
     }
     
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = 1
+        }
+        if greenSwitch.isOn {
+            green = 1
+        }
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
 }
 
