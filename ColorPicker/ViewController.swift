87682738398ea7aea468ccf04ffbe4db5593f4b3
Sema Topcu <ssemaatopcu.@gmail.com>
2024-06-18 19:32:03 +0300

    creating outlets

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
     }
 
 
